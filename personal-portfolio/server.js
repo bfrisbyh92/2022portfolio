0bdcc30c7682c2d86b0f27779ba6b691050a0a8f
Bfrisbyh92 <Bfrisbyh92@gmail.com>
2022-08-27 11:01:05 -0400

    .

--- personal-portfolio/server.js
+++ personal-portfolio/server.js
@@ -9,6 +9,9 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.send((req, res) => {
+  res.send("Portfolio backend up and running!");
+})
 app.use("/", router);
 app.listen(PORT, (conn) => console.log(`Server Running on ${PORT}`));
 console.log(process.env.EMAIL_USER);
